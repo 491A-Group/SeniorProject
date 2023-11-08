d6f6851b3b4e57ae26f1ecbf5d06efe0a06d8a3e
brian <lbc.brian.chilin@gmail.com>
2023-11-08 14:20:29 -0800

    trying to get camera stuff working

--- src/App.js
+++ src/App.js
@@ -12,28 +12,28 @@ function App() {
   }
 
   const webcamRef = React.useRef(null);
-  const [imageSrc, setImageSource] = useState("NoSource");
+  const [imageSrc, setImageSource] = useState("result");
   const capture = useCallback(
     () => {
-      setImageSource(webcamRef.current.getScreenshot());
-      console.log(imageSrc);
-      fetch("https://c927-107-185-101-105.ngrok-free.app/testing")
-  .then(response => {
+      //setImageSource(webcamRef.current.getScreenshot());
+      //console.log(imageSrc);
+      //fetch("https://c927-107-185-101-105.ngrok-free.app/testing")
+      let img_bin = atob(webcamRef.current.getScreenshot())
+      fetch("http://192.168.1.69:3030/predict", {
+        mode:'cors',
+        method: 'POST',
+        body: img_bin
+      })
+      .then(response => {
     if (!response.ok) {
       throw new Error("Network response was not ok");
     }
-    return response.json();
+    return response.json(); 
   })
   .then(data => {
     // Log the entire data object to the console for debugging
-    console.log(data);
-    
-    // Check if the "message" field exists in the data
-    if (data.hasOwnProperty("message")) {
-      setImageSource(data.message);
-    } else {
-      console.error("The 'message' field is missing in the JSON response.");
-    }
+    console.log(55, data);
+    setImageSource(data);
   })
   .catch(error => {
     console.error("There was a problem with the fetch operation:", error);
@@ -47,7 +47,7 @@ function App() {
   return (
     <div className="App">
       <Webcam ref={webcamRef} screenshotFormat="image/jpeg" videoConstraints={vc}/>
-      <button onClick={capture}>click me </button> 
+      <button onClick={capture}>click me!!</button> 
       <p>{imageSrc}</p>
       <Card make="Ferrari" rarity={100} model="250 Testa Rossa" generation="1957-1961"/>
     </div>

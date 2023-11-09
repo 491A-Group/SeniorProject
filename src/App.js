c3a0c9b2340606f915f22219535dc340c22c50d6
Ceweiss42 <ceweiss7@gmail.com>
2023-11-09 11:32:50 -0800

    Isolate Camera page

--- src/App.js
+++ src/App.js
@@ -1,60 +1,28 @@
 import './App.css';
+import CameraPage from './pages/CameraPage';
+import HomePage from './pages/HomePage';
 import React from 'react';
-import Webcam from 'react-webcam';
-import { useState, useCallback} from 'react';
-import {Buffer} from 'buffer';
-import logo from './logo.svg';
-import Card from './components/Card';
-
+import { useState } from 'react';
 
 function App() {
 
-  const vc = {
-    facingMode: { exact: "environment" }
-  }
+  const [activePage, setActivePage] = useState(<HomePage/>)
 
-  const webcamRef = React.useRef(null);
-  const [imageSrc, setImageSource] = useState("");
-  const [predictionResult, setPrediction] = useState("");
-
-  const fetchString = (base64String) => {
-    // Convert base64 to binary, which will later become a uint8 array
-    const binaryData = Buffer.from(
-      base64String.slice(22), //drop the first characters //
-      'base64'
-    );
-
-    // Send the POST request with the image data.
-    fetch('https://sc-prediction-model.brian2002.com/predict', {
-      method: 'POST',
-      body: binaryData,
-    })
-      .then((response) => response.text())
-      .then((data) => {
-        setPrediction(data);
-      })
-      .catch((error) => {
-        console.error('Error fetching data:', error);
-      });
+  const setPage = () => {
+    setActivePage(<CameraPage clientID={123}/>);
   };
-
-  
-  const capture = useCallback(
-    () => {
-      setImageSource(webcamRef.current.getScreenshot());
-      fetchString(imageSrc);      
-    },
-    [webcamRef, fetchString]
-  );
 
 
   return (
-    <div className="App">
-      <Webcam width="244" height="244" ref={webcamRef} screenshotFormat="image/jpeg" videoConstraints={vc}/>
-      <button onClick={capture}>Take Photo</button> 
-      <p>{predictionResult}</p>
+    <div>
+      {activePage}
+      <div>
+        <p>This is the navBar</p>
+        <button onClick={setPage}>Camera</button>
+      </div>
     </div>
   );
+  
 }
 
 //<Card make="Ferrari" rarity={100} model="250 Testa Rossa" generation="1957-1961"/>

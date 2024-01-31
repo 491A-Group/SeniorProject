69bfe23f3301bb85a0d49f7728b2ff860d5a7e5d
Ceweiss42 <ceweiss7@gmail.com>
2024-01-31 12:29:59 -0800

    Created LoginPage

---
+++ src/pages/LoginPage.js
@@ -0,0 +1,14 @@
+import './CameraPage.css';
+import React from 'react';
+
+export default function LoginPage(){
+    return(
+        //what the login page will return
+
+        <div>
+            <button  className="signin" onClick={null}>Sign In</button>
+            <button className="register" onClick={null}>Create Account</button>
+
+        </div>
+    )
+};

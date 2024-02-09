7b7fc98508ff6d94a7a2c91fc646ae21bd284f73
Richard Lee <73311642+RichardLee19@users.noreply.github.com>
2024-02-08 18:38:34 -0800

    Added LoginPage.css and .js files

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,26 +1,62 @@
-import './CameraPage.css';
-import React from 'react';
+import React, { useState } from 'react';
 
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-  Link,
-} from "react-router-dom";
 
-export default function LoginPage(){
+function LoginPage() {
+    const [isSignIn, setIsSignIn] = useState(true); 
+    const [email, setEmail] = useState('');
 
-    return(
+    const handleToggleMode = () => {
+        setIsSignIn(!isSignIn);
+    };
 
-        <div className="bg">
-            <img src=""/>
+    const handleEmailChange = (e) => {
+        setEmail(e.target.value);
+    };
+
+    return (
+        <div className="container">
+            <h1>{"Welcome to SportsCar Spotter"}</h1>
+            {
+            //Since value is set to True, will default to Sign in. Else, header changes to Create Account.
+            }
+            <h2>{isSignIn ? 'Sign In' : 'Create Account'}</h2> 
+
+            {
+            //Conditional Rendering Statement. If !iSignIn (If we are in the Create Account Page), display
+            //an extra text box to input an email address. 
+            }
+
+            { !isSignIn && (
+                <div className="input-group">
+                    <input type="email" placeholder="Email" value={email} onChange={handleEmailChange} />
+                </div>
+            )}
+            <div className="input-group">
+                <input type="text" placeholder="Username" />
+            </div>
+            <div className="input-group">
+                <input type="password" placeholder="Password" />
+            </div>
+            { isSignIn && (
+                <button className="btn">{'Forgot Password?'}</button>
+            )}
+            { isSignIn && (
+                <br />
+            )}
             
-            <Link to="/home">
-                <button  className="signin" onClick={null}>Sign In</button>
-            </Link>
-            <button className="register" onClick={null}>Create Account</button>
+    
+            {
+            // if isSignIn is True, show 'Sign In'; else show 'Create Account'.
+            }
+            <button className="btn">{isSignIn ? 'Sign In' : 'Create Account'}</button>
 
+            <p>{isSignIn ? "Don't have an account? " : "Already have an account? "} 
+               <button className="btn" onClick={handleToggleMode}>
+                   {isSignIn ? 'Create Account' : 'Sign In'}
+               </button>
+            </p>
         </div>
-    )
-};
+    );
+}
+
+export default LoginPage;

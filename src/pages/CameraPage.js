07de3238273b77aee06e95cb65a1f297eaf9c214
brian <lbc.brian.chilin@gmail.com>
2024-02-10 12:02:58 -0800

    got rid of all references to old server. please fix camera
    camera doesn't show up in app anymore? i changed the url of the post request but i dont even see the camera. it wasn't working before i touched it

--- src/pages/CameraPage.js
+++ src/pages/CameraPage.js
@@ -9,77 +9,76 @@ import Garage from "../images/garage.png";
 import Home from "../images/home.png";
 
 
-export default function CameraPage({clientID}) {
+export default function CameraPage() {
 
-    const vc = {
-        facingMode: { exact: "environment" }
-      }
-    
-      const webcamRef = React.useRef(null);
-      const [imageSrc, setImageSource] = useState("");
-      const [predictionResult, setPrediction] = useState("Please take a photo...");
-    
-      //main function for prediction communication
-      //sends image data over to server
-      //gets string prediction back
-      const fetchString = (base64String) => {
-        // Convert base64 to binary, which will later become a uint8 array
-        const binaryData = Buffer.from(
-          base64String.slice(22), //drop the first characters //
-          'base64'
-        );
+  const vc = {
+      facingMode: { exact: "environment" }
+  }
+  
+  const webcamRef = React.useRef(null);
+  const [imageSrc, setImageSource] = useState("");
+  const [predictionResult, setPrediction] = useState("Please take a photo...");
 
-        // Send the POST request with the image data.
-        fetch('https://sc-prediction-model.brian2002.com/predict', {
-          method: 'POST',
-          body: binaryData
-        })
-          .then((response) => response.text())
-          .then((data) => {
-            //get the prediction from the server, set the variable
-            setPrediction(data);
-          })
-          .catch((error) => {
-            console.error('Error fetching data:', error);
-          });
-      };
-    
-      //function for getting the screenshot
-      //automatically sends prediction to the server
-      const capture = useCallback(
-        () => {
+  //main function for prediction communication
+  //sends image data over to server
+  //gets string prediction back
+  const fetchString = (base64String) => {
+    // Convert base64 to binary, which will later become a uint8 array
+    const binaryData = Buffer.from(
+      base64String.slice(22), //drop the first characters
+      'base64'
+    );
 
-          //get screenshot
-          setImageSource(webcamRef.current.getScreenshot());
+    // Send the POST request with the image data.
+    fetch('https://sc-backend.brian2002.com/predict', {
+      method: 'POST',
+      body: binaryData
+    })
+    .then((response) => response.text())
+    .then((data) => {
+      //get the prediction from the server, set the variable
+      setPrediction(data);
+    })
+    .catch((error) => {
+      console.error('Error fetching data:', error);
+    });
+  };
 
-          //send screenshot, get prediction
-          fetchString(imageSrc);      
-        },
+  //function for getting the screenshot
+  //automatically sends prediction to the server
+  const capture = useCallback(
+    () => {
+      //get screenshot
+      setImageSource(webcamRef.current.getScreenshot());
 
-        //references used in callback
-        //anything that the callback needs to "pay attention" to needs to be here
-        [webcamRef, fetchString]
-      );
-    
-    
-      //returns the main camera page to be displayed
-      return (
-        <div className="CameraPage">
-          <Webcam margin-left="-20px"width="100%" height="100%" ref={webcamRef} screenshotFormat="image/jpeg" videoConstraints={vc}/>
-          <button  onClick={capture}>Take Photo</button> 
-          <p>{predictionResult}</p>
-          <div className='spacer'/>
-          <div className="nav">
-            <button onClick={null}>
-              <img src={Home}/>
-            </button>
-            <button onClick={null}>
-              <img src={Camera}/>
-            </button>
-            <button onClick={null}>
-              <img src={Garage}/>
-            </button>
-          </div>
-        </div>
-      );
+      //send screenshot, get prediction
+      fetchString(imageSrc);      
+    },
+
+    //references used in callback
+    //anything that the callback needs to "pay attention" to needs to be here
+    [webcamRef, fetchString]
+  );
+
+
+  //returns the main camera page to be displayed
+  return (
+    <div className="CameraPage">
+      <Webcam margin-left="-20px"width="100%" height="100%" ref={webcamRef} screenshotFormat="image/jpeg" videoConstraints={vc}/>
+      <button onClick={capture}>Take Photo</button> 
+      <p>{predictionResult}</p>
+      <div className='spacer'/>
+      <div className="nav">
+        <button onClick={null}>
+          <img src={Home}/>
+        </button>
+        <button onClick={null}>
+          <img src={Camera}/>
+        </button>
+        <button onClick={null}>
+          <img src={Garage}/>
+        </button>
+      </div>
+    </div>
+  );
 };
--- src/pages/TestPage.js
+++ src/pages/TestPage.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import LoginPage from './LoginPage';
+import GaragePage from './GaragePage';
+import CameraPage from './CameraPage';
 
 
 export default function TestPage({setAppPage}) {
-    const changePage = () =>
-    {
-        setAppPage(<LoginPage setAppPage={setAppPage}/>)
-    }
+  const to_login = () => {
+    setAppPage(<LoginPage setAppPage={setAppPage}/>)
+  }
+
+  const to_garage = () => {
+    setAppPage(<GaragePage/>)
+  }
+
+  const to_camera = () => {
+    setAppPage(<CameraPage/>)
+  }
 
   return (
     <div>
-        <button onClick={changePage}>Click me!</button>
+      <button onClick={to_login}>To Login</button>
+      <button onClick={to_garage}>To Garage</button>
+      <button onClick={to_camera}>To Camera</button>
     </div>
   );
   

1e098af9dac5cc32c017b4d0ace2ce2a2ef7dd39
Ceweiss42 <ceweiss7@gmail.com>
2024-02-17 00:47:47 -0800

    Update CatchPage.js

--- frontend/src/pages/CatchPage.js
+++ frontend/src/pages/CatchPage.js
@@ -36,8 +36,8 @@ export default function CatchPage({changePage}) {
   };
 
   useEffect(() => {
-    console.log(localStorage.getItem("imageBase"));
-    fetchString(localStorage.getItem("imageBase"));
+    let imageString = await(localStorage.getItem("imageBase"));
+    fetchString(imageString);
   }, []);
 
     //the main return to display the home page or main feed

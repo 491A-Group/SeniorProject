fd4bbc85c9bab040d1640807d6a306990db64820
JayveeHenzon <jayveelou.henzon@student.csulb.edu>
2024-02-16 23:58:10 -0800

    added comments to HomePage.js

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,42 +1,72 @@
+// Jayvee wrote most of this file unless denoted otherwise
+
 import React, { useState, useEffect } from 'react';
 import './HomePage.css';
+import Search from "../images/search.png";
+import Filter  from "../images/filter.png";
 // import CarData from "./CarData.json";
 // import Ferrari from "../images/ferrari.png";
 // import Testa from "../images/testa_rossa.jpeg";
 // import Ford from "../images/ford.png";
 // import FordGT from "../images/ford-gt40.jpg";
-import Search from "../images/search.png";
-import Filter  from "../images/filter.png";
 // import Porsche from "../images/porsche-_car.jpg";
 // import PorLogo from "../images/porsche.png";
 
+// Jayvee
+// Overall Main/Home page
 export default function HomePage({changePage}) {
+
+    // Jayvee
+    // Initializing a state variable 'carData' using useState hook with an empty array as initial state.
     const [carData, setCardata] = useState([]);
 
+    // Jayvee
+    // useEffect hook to perform side effects like data fetching when the component mounts.
     useEffect(() => {
+
+      // Jayvee
+      // Async function to fetch data from the specified endpoint.
       const fetchData = async () => {
         try {
+
+         // Jayvee
+         // Fetching data from the provided API endpoint.
          const response =  await fetch(window.location.origin + '/api/home_1');
+
+         // Jayvee
+         // Checking if the response is okay, if not, logging a network error and throwing an error.
           if (!response.ok) {
             console.log("network error")
             throw new Error('Failed to fetch data');
           }
+
+          // Jayvee
+          // Parsing the response data to JSON format.
           const jsonData = await response.json();
 
+          // Jayvee
+          // Updating the 'carData' state with the fetched JSON data.
           setCardata(jsonData);
 
         } catch(error){
+          // Jayvee
+          // Catching any errors that occur during the fetch process and logging them.
             console.error('Error fetching data', error);
         }
       };
      
+      // Jayvee
+      // Calling the fetchData function when the component mounts.
       fetchData();
-    }, []);
+    }, []); // Empty dependency array ensures that this effect runs only once after the initial render.
 
+    // Cameron
+    // Function to navigate to the 'Test' page when called.
     const toTest = () => {
       changePage("Test");
     }
   
+    //Jayvee
     //the main return to display the home page or main feed
     return (
       <div>

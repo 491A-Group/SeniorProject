fa4c3836d33cf4fb378c52f6a7a37e8137d1d7d4
Ceweiss42 <ceweiss7@gmail.com>
2024-02-17 01:09:04 -0800

    Update CatchPage.js

--- frontend/src/pages/CatchPage.js
+++ frontend/src/pages/CatchPage.js
@@ -7,22 +7,22 @@ export default function CatchPage({changePage}) {
   const [prediction, setPrediction] = useState("");
 
   const fetchString = (base64String) => {
-    const binaryData = Buffer.from(
-      base64String.slice(22), // Drop the first characters
-      'base64'
-    );
+    // const binaryData = Buffer.from(
+    //   base64String.slice(22), // Drop the first characters
+    //   'base64'
+    // );
     
-    // Create a Blob object from the binary data
-    const blobData = new Blob([binaryData]);
+    // // Create a Blob object from the binary data
+    // const blobData = new Blob([binaryData]);
     
-    // Create a FormData object and append the Blob data to it
-    const formData = new FormData();
-    formData.append('file', blobData, 'filename.jpg'); // 'file' is the name of the form field
+    // // Create a FormData object and append the Blob data to it
+    // const formData = new FormData();
+    // formData.append('file', blobData, 'filename.jpg'); // 'file' is the name of the form field
     
     // Send the POST request with the FormData object
     fetch('https://sc-backend.brian2002.com/predict', {
       method: 'POST',
-      body: formData
+      body: base64String
     })
     .then((response) => response.text())
     .then((data) => {

124bb3f1c00ac4c486521c1dc7c1571447882848
Ceweiss42 <ceweiss7@gmail.com>
2024-02-16 18:11:06 -0800

    Started Catch Page Request

--- frontend/src/pages/CameraPage.js
+++ frontend/src/pages/CameraPage.js
@@ -8,6 +8,7 @@ import Camera from "../images/camera.png";
 import Garage from "../images/garage.png";
 import Home from "../images/home.png";
 import Circle from "../images/circle-100.png";
+import CatchPage from './CatchPage';
 
 export default function CameraPage({changePage, clientID}) {
 
@@ -30,40 +31,19 @@ export default function CameraPage({changePage, clientID}) {
   //main function for prediction communication
   //sends image data over to server
   //gets string prediction back
-  const fetchString = (base64String) => {
-    console.log("Got the string, now fetching...");
-    // Convert base64 to binary, which will later become a uint8 array
-    const binaryData = Buffer.from(
-      base64String.slice(22), //drop the first characters
-      'base64'
-    );
-    // Send the POST request with the image data.
-    fetch('https://sc-backend.brian2002.com/predict', {
-      method: 'POST',
-      body: binaryData
-    })
-    .then((response) => response.text())
-    .then((data) => {
-      //get the prediction from the server, set the variable
-      //setPrediction(data);
-      console.log(data);
-    })
-    .catch((error) => {
-      console.error('Error fetching data:', error);
-    });
-  };
+  
 
   //function for getting the screenshot
   //automatically sends prediction to the server
   const capture = useCallback(
     () => {
       //get screenshot
-      console.log("IS THIS WORKING??");
       setImageSource(webcamRef.current.getScreenshot());
         //references used in callback
         //anything that the callback needs to "pay attention" to needs to be here
+        localStorage.setItem("imageBase", imageSrc);
 
-      fetchString(imageSrc);
+      changePage("Catch");
     },
     [webcamRef]
     );
--- frontend/src/pages/CatchPage.js
+++ frontend/src/pages/CatchPage.js
@@ -1,10 +1,43 @@
+import { useEffect } from 'react';
+import { useSearchParams } from 'react-router-dom';
 import './HomePage.css';
 
-export default function CatchPage({changePage, imageString}) {
+export default function CatchPage({changePage}) {
+
+  const [prediction, setPrediction] = useState("");
+
+  const fetchString = (base64String) => {
+    console.log("Got the string, now fetching...");
+    // Convert base64 to binary, which will later become a uint8 array
+    const binaryData = Buffer.from(
+      base64String.slice(22), //drop the first characters
+      'base64'
+    );
+    // Send the POST request with the image data.
+    fetch('https://sc-backend.brian2002.com/predict', {
+      method: 'POST',
+      body: binaryData
+    })
+    .then((response) => response.text())
+    .then((data) => {
+      //get the prediction from the server, set the variable
+      setPrediction(data);
+      console.log(data);
+    })
+    .catch((error) => {
+      console.error('Error fetching data:', error);
+    });
+  };
+
+  useEffect(() => {
+    fetchString(localStorage.getItem("imageBase"));
+  }, []);
+
     //the main return to display the home page or main feed
     return (
       <div>
-        <p>{imageString}</p>
+        <p>Welcome to the Catch Page. Our prediction is...</p>
+        {prediction}
       </div>
     );
   }

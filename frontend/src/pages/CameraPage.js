5405129998e93350456934c9121f05c85222a01d
Ceweiss42 <ceweiss7@gmail.com>
2024-02-17 01:31:36 -0800

    its too early

--- frontend/src/pages/CameraPage.js
+++ frontend/src/pages/CameraPage.js
@@ -48,6 +48,7 @@ export default function CameraPage({changePage}) {
   const capture = useCallback(
     () => {
         setImageSource(webcamRef.current.getScreenshot());
+        localStorage.setItem("imageBase", imageSrc);
         toCatch();
       
     },

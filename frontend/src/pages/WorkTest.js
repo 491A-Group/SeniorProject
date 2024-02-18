16ecc33fb96d3bdc1edf419a3590f4a5c11c1f3f
Ceweiss42 <ceweiss7@gmail.com>
2024-02-17 17:21:24 -0800

    Update WorkTest.js

--- frontend/src/pages/WorkTest.js
+++ frontend/src/pages/WorkTest.js
@@ -4,6 +4,16 @@ function WorkTest({ changePage, iSource, setSource}) {
 
     const changeSource = () => {
         setSource(iSource + "A");
+        fetch('https://sc-backend.brian2002.com/predict', {
+            method: 'POST',
+            body: iSource
+            })
+            .then((response) => response.text())
+            .then((data) => {
+            })
+            .catch((error) => {
+            console.error('Error fetching data:', error);
+            });
     }
   return (
     <div>

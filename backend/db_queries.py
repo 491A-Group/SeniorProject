acb1c5ac4f0f1063c068072dc2edac29e32ec58e
brian <lbc.brian.chilin@gmail.com>
2024-02-20 17:24:25 -0800

    Revert "refactor bad connection try with 'with getconn() as conn' statements"
    This reverts commit f7490b2ae708a2f7644fec5592b9a7672a47e7a6.

--- backend/db_queries.py
+++ backend/db_queries.py
@@ -46,7 +46,9 @@ def verify_credentials(email, raw_password):
         3. argon2 verify password hash from SQL return
         4. determines http code to return
     """
-    with db_connection_pool.getconn() as connection:
+
+    connection = db_connection_pool.getconn()
+    try:
         with connection.cursor() as cursor:
             cursor.execute("""  SELECT id, password
                                 FROM users
@@ -65,6 +67,9 @@ def verify_credentials(email, raw_password):
                     return "Log In Success", 202, id
                 except Exception as e:
                     print("\nfailed\n", e)
+    finally:
+        print("returning a thread to the pool")
+        db_connection_pool.putconn(connection)
 
     return "Log In Rejected", 401
 
@@ -77,7 +82,8 @@ def register_credentials(email, displayname, raw_password):
 
     # TODO FIX EXCEPTIONS IE DUPLICATE EMAIL, DUPLICATE DISPLAYNAME
     """
-    with db_connection_pool.getconn() as connection:
+    connection = db_connection_pool.getconn()
+    try:
         with connection.cursor() as cursor:
             cursor.execute("""  INSERT INTO users (email, displayname, password)
                                 VALUES (%s, %s, %s)
@@ -96,7 +102,9 @@ def register_credentials(email, displayname, raw_password):
                 connection.commit()
                 return 'Registration Success', 201, query_result[0]
             return 'Registration Failed', 409
-
+    finally:
+        print("returning a thread to the pool")
+        db_connection_pool.putconn(connection)
 
 def follows(user_id):
     """ BRIAN
@@ -105,7 +113,8 @@ def follows(user_id):
     OR if ID isn't found:
         (-2, -2)
     """
-    with db_connection_pool.getconn() as connection:
+    connection = db_connection_pool.getconn()
+    try:
         with connection.cursor() as cursor:
             cursor.execute("""  SELECT
                                     (SELECT COUNT(*) FROM follows WHERE follower = %s),
@@ -119,3 +128,6 @@ def follows(user_id):
             if query_result is not None:
                 return query_result
             return -2, -2
+    finally:
+        print("returning a thread to the pool")
+        db_connection_pool.putconn(connection)

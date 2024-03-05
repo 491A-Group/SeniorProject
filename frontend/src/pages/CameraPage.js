0ca7ce52e63203ee9bebd4069ce3288bc5b935df
Ceweiss42 <ceweiss7@gmail.com>
2024-03-05 14:17:56 -0800

    Update CameraPage.js

--- frontend/src/pages/CameraPage.js
+++ frontend/src/pages/CameraPage.js
@@ -20,8 +20,6 @@ export default function CameraPage() {
     //specs for the webcam module, makes the camera be the forward facing camera
     const vc = {
         facingMode: { exact: "environment" },
-        width: "400px",
-        height: "400px"
     }
     //cameron
     //THIS FUNCTION FORCES THE PAGE TO NOT SCROLL AT ALL

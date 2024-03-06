9a5cb7def12db8d4830807abb58b04bc5a0549f4
brian <lbc.brian.chilin@gmail.com>
2024-03-05 19:57:10 -0800

    comment RenderUserList.js

--- frontend/src/components/RenderUserList.js
+++ frontend/src/components/RenderUserList.js
@@ -3,6 +3,9 @@ import "./RenderUserList.css"
 import { useNavigate } from 'react-router-dom';
 import DefaultProfilePicture from '../images/DefaultProfilePicture.png';
 
+// this component takes a state thats a list of objects. each object is a user with profile picture and displayname
+//    this is to be used in at least 3 locations: search, followers, following. it's useful to keep them styled similarly
+
 export default function RenderUserList({users}) {
     const navigate = useNavigate();
     return <div>

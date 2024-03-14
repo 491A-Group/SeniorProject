0a7592e3be93246a5583b80c8d6ccffb7ee00fe8
Richard Lee <73311642+RichardLee19@users.noreply.github.com>
2024-03-14 11:58:28 -0700

    NavBar styling

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -22,19 +22,19 @@ export default function NavBar() {
     return (
         <div className="navbar">
             <button onClick={() => handleNavigation("/home")} className={`nbtn ${location.pathname === "/home" ? "active" : ""}`}>
-                <img width="50%" src={Home} alt="Home Page"/>
+                <img width="60%" src={Home} alt="Home Page"/>
             </button>
             <button onClick={() => handleNavigation("/search")} className={`nrej ${location.pathname === "/search" ? "active" : ""}`}>
-                <img width="50%" src={search} alt="Search Page"/>
+                <img width="60%" src={search} alt="Search Page"/>
             </button>
-            <button onClick={() => handleNavigation("/camera")} className={`nbtn ${location.pathname === "/camera" ? "active" : ""}`}>
-                <img width="50%" src={camera} alt="Begin Identification"/>
+            <button onClick={() => handleNavigation("/camera")} className={`nrej ${location.pathname === "/camera" ? "active" : ""}`}>
+                <img width="60%" src={camera} alt="Begin Identification"/>
             </button>
             <button onClick={() => handleNavigation("/daily")} className={`nrej ${location.pathname === "/daily" ? "active" : ""}`}>
-                <img width="50%" src={puzzle} alt="Daily Event Page"/>
+                <img width="60%" src={puzzle} alt="Daily Event Page"/>
             </button>
             <button onClick={() => handleNavigation("/garage")} className={`nbtn ${location.pathname === "/garage" ? "active" : ""}`}>
-                <img width="50%" src={Garage} alt="Garage Page"/>
+                <img width="60%" src={Garage} alt="Garage Page"/>
             </button>
         </div>
     );

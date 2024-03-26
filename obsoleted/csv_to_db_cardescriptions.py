ad368c4c30815d7968ea3bec8e3c443db3f962c7
brian <lbc.brian.chilin@gmail.com>
2024-03-26 12:50:10 -0700

    fixed description script bug

--- obsoleted/csv_to_db_cardescriptions.py
+++ obsoleted/csv_to_db_cardescriptions.py
@@ -11,8 +11,9 @@ with open(file_path, 'r') as file:
 
         for line in file:
             label, description = line.split(',', 1)
-            description = description.strip('"')
-            print("label:!" + label + "!\ndescription:" + description)
+            description = description.strip() # remove whitespaces/newlines
+            description = description.strip('"') # remove quotes
+            print("label:!" + label + "!\ndescription:" + description + "!")
             cur.execute(
                 "UPDATE cars SET description=%s WHERE label ILIKE %s;",
                 (description, label)

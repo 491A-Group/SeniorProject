ac94f29a31ccd928592fbd35e2eb369b12486900
JayveeHenzon <jayveelou.henzon@student.csulb.edu>
2024-03-28 11:54:24 -0700

    Added generic logo to main feed

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -9,6 +9,7 @@ import NavBar from '../components/NavBar';
 import Post from '../components/Post';
 import heart from '../images/heart.png';
 import TestPage from './TestPage';
+import Logo from '../images/genericLogo.png';
 //Le Duong, installed react infinite scroll dependency in frontend node_modules folder, might need to install locally
 import InfiniteScroll from 'react-infinite-scroll-component';
 
@@ -134,6 +135,7 @@ export default function HomePage() {
       <div className="homeContainer">
         <TestPage/>
         <div>
+          <img src={Logo} alt="Logo" style={{ marginRight: '10px'}} />
           <h1 style={{color: 'red'}}>Sportscar Spotter</h1> {/* HomePage Title */}
         <div className="searchPad">
           <img src={Filter} alt="Filter" /> {/* Filter icon */}

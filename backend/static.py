405eedb91203a32a7a49d73a05c7acbd43d87550
brian <lbc.brian.chilin@gmail.com>
2024-04-25 02:55:14 -0700

    pfp: bad backend code and worse frontend code

--- backend/static.py
+++ backend/static.py
@@ -9,53 +9,92 @@ from backend.db_queries import db_connection_pool
 
 blueprint_db_static = Blueprint("blueprint_db_static", __name__)
 
+def send_svg(svg):
+    return send_file(
+        BytesIO(svg),
+        mimetype='image/svg+xml'
+    )
+
+# BRIAN: setting up a read-only cache for
+#   profile pictures. This block of codes only runs once when this module is first loaded
+print('loading pfp caches...')
+import threading
+pfp_groups = {}             #entry point, group_name is manufacturer name;       group_name: (manu_id, quantity)
+pfp_cache_by_manu_id = {}   #manufacturer_id: [pfp_id1, pfp_id2, ..., pfp_idN]
+pfp_cache_by_id = {}        #id: svg
+pfp_cache_lock = threading.Lock()
+# the following variables are only for use in the error cases in static.py
+# other places like select pfp menu s can just find them lost in the dictionaries
+DEFAULT_USER_SVG = None 
+DEFAULT_MANU_SVG = None 
+with pfp_cache_lock:
+    results = None
+    with db_connection_pool.connection() as conn:
+        cursor = conn.execute(
+            """
+            SELECT p.id, p.img, m.id, m.name
+            FROM profile_pictures p
+            LEFT JOIN manufacturers m ON m.id = p.manufacturer_id
+            ORDER BY p.id
+            """
+        )
+        results = cursor.fetchall()
+        if not results:
+            print("\n\n!!Error fetching profile pictures upon startup!!\n")
+        for index, result in enumerate(results):
+            if result[2] is None:
+                results[index] = (result[0], result[1], 0, 'Other') # fix the manu id and manu name. tuple doesn't allow modifying those values, so reassign the whole tuple
+        # this comment is the end of cleaning up the results from the db query
+        for pfp_id, pfp_svg, manu_id, manu_name in results:
+            # this first if statement catches special cases; default pictures
+            if pfp_id == 1:
+                DEFAULT_USER_SVG = pfp_svg
+            elif pfp_id == 96:
+                DEFAULT_MANU_SVG = pfp_svg
+
+            pfp_cache_by_id[pfp_id] = pfp_svg
+            if manu_id not in pfp_cache_by_manu_id:
+                pfp_groups[manu_name] = [manu_id, 0] # list while creating for easy mutability, turn to tuple later. (id, quantity)
+                pfp_cache_by_manu_id[manu_id] = []
+            pfp_groups[manu_name][1] += 1
+            pfp_cache_by_manu_id[manu_id].append(pfp_id) 
+    for key, values_list in pfp_groups.items():
+        pfp_groups[key] = tuple(values_list)
+print('done loading pfp caches.', len(pfp_groups), len(pfp_cache_by_manu_id), len(pfp_cache_by_id))
+# END OF INIT STATIC CACHE
+
+
+
 @blueprint_db_static.route('/pfp/<id>', methods=['GET'])
 def pfp(id):
     """BRIAN:
     Serve profile pictures by id. In the event an invalid id is provided it serves the one with ID 1 - the default. 
     """
-    with db_connection_pool.connection() as conn:
-        id = id[:9] # get the first 9 characters. max id becomes 999,999,999
-        try:
-            id = int(id)
-        except ValueError:
-            id = 1
-        cursor = conn.execute("""  
-                            SELECT COALESCE(
-                                (SELECT img FROM profile_pictures WHERE id = %s),
-                                (SELECT img FROM profile_pictures WHERE id = 1)
-                            )"""
-                        , (id,))
-        return send_file(
-            BytesIO(cursor.fetchone()[0]),
-            mimetype='image/svg+xml'
-        )
+    id = id[:9] # get the first 9 characters. max id becomes 999,999,999
+    try:
+        id = int(id)
+    except ValueError:
+        return send_svg(DEFAULT_USER_SVG)
+    
+    if id in pfp_cache_by_id:
+        return send_svg(pfp_cache_by_id[id])
+    return send_svg(DEFAULT_USER_SVG)
         
 @blueprint_db_static.route('/brand/<id>/logo.svg', methods=['GET'])
 def brand_logo(id):
     """BRIAN:
     Serve brand pictures by brand id. In the event an invalid id is provided it serves the one with ID 96 - the factory.
     """
-    with db_connection_pool.connection() as conn:
-        id = id[:8] # get the first 8 characters. max id becomes 99,999,999
-        try:
-            id = int(id)
-        except ValueError:
-            id = 96
-        cursor = conn.execute("""  
-                            SELECT COALESCE(
-                                (   
-                                    SELECT img 
-                                    FROM profile_pictures p 
-                                    JOIN manufacturers m ON m.id=p.manufacturer_id
-                                    WHERE m.id=%s
-                                    ORDER BY p.id
-                                    LIMIT 1
-                                ),
-                                (SELECT img FROM profile_pictures WHERE id = 96)
-                            )"""
-                        , (id,))
-        return send_file(
-            BytesIO(cursor.fetchone()[0]),
-            mimetype='image/svg+xml'
-        )
+    id = id[:8] # get the first 8 characters. max id becomes 99,999,999
+    try:
+        id = int(id)
+    except ValueError:
+        return send_svg(DEFAULT_MANU_SVG)
+
+    if id in pfp_cache_by_manu_id:
+        # pfp_cache_by_manu_id[id] gives the list of id's of pictures for that manufacturer
+        #   so pfp_cache_by_manu_id[id][0] is the ID of the first picture for that manufacturer
+        return send_svg(pfp_cache_by_id[pfp_cache_by_manu_id[id][0]])
+    
+    return send_svg(DEFAULT_MANU_SVG)
+    
--- frontend/src/pages/SettingsPFP.js
+++ frontend/src/pages/SettingsPFP.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import TestPage from './TestPage';
 import './FeatureRequest.css';
@@ -7,12 +7,122 @@ import BackButton from '../components/BackButton';
 const SettingsPFP = () => {
     const navigate = useNavigate();
 
+    /*
+        result of /settings/pfp
+        pfp groups will have form like
+
+        {
+            "Acura": [id, quantity_logos],
+            "Aston Martin": [8, 2],
+            "Other": [0, 84]
+            ...
+        }
+    */
+    const [pfpGroups, setPfpGroups] = useState({})
     
+    /*
+        result of /settings/pfp/<manufacturer_id>
+        simply a list of id's of profile pictures
+    */
+    const [brandPfpIds, setBrandPfpIds] = useState([])
+    
+    
+    /*
+        THIS DEBUG GARBAGE IS JUST TO PLAY AROUND WITH
+        ONCE YOU ARE FAMILIAR WITH THE SYSTEM GET RID OF THESE DUMB INPUT FIELDS
+
+        INSTEAD EVERYTHING WILL BE BUTTONS
+        we know the id of all the brands and all the ids of the pictures..
+        its given in the request before right
+
+        so this is just a way to play around but get rid of all this following code for input fields
+    */
+    const [manufactureInput, setManufactureInput] = useState('')
+    const [pfpIdInput, setPfpIdInput] = useState('') // backend endpoint might be able to handle string OR int
+    const handleManuChange = (event) => {
+        setManufactureInput(event.target.value);
+    };
+    const handlePfpChange = (event) => {
+        setPfpIdInput(event.target.value);
+    };
+
+
+    /*some of the following code is actually remotely salvageable*/
+    async function get_pfp_groups_request() {
+        const response = await fetch(window.location.origin + '/settings/pfp')
+        setPfpGroups(await response.json())
+    }
+    async function get_manufacturer_pfps() {
+        const response = await fetch(window.location.origin + '/settings/pfp/' + manufactureInput)
+        setBrandPfpIds(await response.json())
+        //maybe fix this function to handle bad responses:
+        //response may return like 'Invalid manufacturer', 404
+        // truthfully you should only be sending valid manufacturer id's anyways
+    }
+    async function submit_new_pfp() {
+        const response = await fetch(
+            window.location.origin + '/settings/pfp',
+            {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({
+                    pfp: parseInt(pfpIdInput)
+                }),
+            }
+        )
+        // can get status from response... i didnt feel like it for this debug just check garage to see new pfp
+    }
+
     return (
         <>
             <BackButton enableBackButton={true} />
             <div className="container">
                 <h1 classname = "h1">Change Profile Picture</h1>
+            </div>
+
+
+
+            <div className="container">
+                <h1 classname = "h1">entry request example</h1>
+                <button onClick={get_pfp_groups_request}>get pfp groups</button>
+                <p>
+                    {Object.keys(pfpGroups).map((key) => (
+                        <li key={key}>
+                            {key}:<br/>
+                            id {pfpGroups[key][0]}<br/>
+                            quantity {pfpGroups[key][1]}
+                        </li>
+                    ))}
+                </p>
+            </div>
+            <div className="container">
+                <h1 classname = "h1">manufacture request example</h1>
+
+                <input 
+                    type="text"
+                    value={manufactureInput}
+                    onChange={handleManuChange}
+                    placeholder="manufacture id"
+                />
+                <button onClick={get_manufacturer_pfps}>get manufacture pfp list</button>
+
+                <p>
+                    {brandPfpIds.map((pfp_id, index) => (
+                        <p>{pfp_id}</p>
+                    ))}
+                </p>
+            </div>
+            <div className="container">
+                <h1 classname = "h1">post pfp example</h1>
+
+                <input 
+                    type="text"
+                    value={pfpIdInput}
+                    onChange={handlePfpChange}
+                    placeholder="pfp id"
+                />
+                <button onClick={submit_new_pfp}>post new pfp</button>
+
             </div>
         </>
     );
